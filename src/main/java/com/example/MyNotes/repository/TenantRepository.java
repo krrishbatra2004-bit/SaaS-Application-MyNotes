1a364f3138ed5cc33b47019587f1e1b6d8ac8d22
Krrish Batra <krrish.batra.2004@gmail.com>
2026-03-27 02:50:57 +0530

    Delete src directory

--- src/main/java/com/example/MyNotes/repository/TenantRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.MyNotes.repository;
-import com.example.MyNotes.model.Tenant;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface TenantRepository extends JpaRepository<Tenant, Long> {
-}
